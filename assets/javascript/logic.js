d0c41ebcc283e16fe61bab62cc53df564379dc07
Hoo Man <hoomanfor@gmail.com>
2019-10-19 19:30:25 -0400

    when new game starts selection will not trigger

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -13,30 +13,60 @@ firebase.initializeApp(firebaseConfig);
 
 var database = firebase.database();
 
+var p1Wins = 0
+var p2Wins = 0;
+var p1Losses = 0
+var p2Losses = 0
+var ties = 0;
+
 function determineWinner() {
   database.ref("players").once("value", function(snapshot) {
+    p1Wins = snapshot.val().one.win;
+    p2Wins = snapshot.val().two.win;
+    p1Losses = snapshot.val().one.loss;
+    p2Losses = snapshot.val().two.loss;
+    ties = snapshot.val().one.tie;
+    $("figure").css("background-color", "");
     console.log("snapshot.val()", snapshot.val());
     var playerOneSelection = snapshot.val().one.selection;
     console.log("TEST!1", playerOneSelection);
     var playerTwoSelection = snapshot.val().two.selection;
     console.log("TEST!2", playerTwoSelection);
     if (playerOneSelection === "rock" && playerTwoSelection === "rock") {
+      tie()
+      clearSelection()
       console.log("Rock & Rock = Tie")
     } else if (playerOneSelection === "paper" && playerTwoSelection === "paper") {
+      tie()
+      clearSelection()
       console.log("Paper & Paper = Tie")
     } else if (playerOneSelection === "scissors" && playerTwoSelection === "scissors") {
+      tie()
+      clearSelection()
       console.log("Scissors & Scissors = Tie")
     } else if (playerOneSelection === "rock" && playerTwoSelection === "scissors") {
+      p1Win()
+      clearSelection()
       console.log("rock & scissors = Player One Wins!")
     } else if (playerOneSelection === "rock" && playerTwoSelection === "paper") {
+      p2Win()
+      clearSelection()
       console.log("rock & paper = Player Two Wins!")
     } else if (playerOneSelection === "scissors" && playerTwoSelection === "rock") {
+      p2Win()
+      clearSelection()
       console.log("scissors & rock = Player Two Wins!")
     } else if (playerOneSelection === "scissors" && playerTwoSelection === "paper") {
+      p1Win()
+      clearSelection()
       console.log("scissors & paper = Player One Wins!")
     } else if (playerOneSelection === "paper" && playerTwoSelection === "rock") {
+      p1Win()
+      clearSelection()
       console.log("paper & rock = Player One Wins!")
-    } else if (playerOneSelection === "paper" && playerTwoSelection === "scissors") {
+    } else if (playerOneSelection === "paper" && playerTwoSelection === "scissors") { 
+      p2Win()
+      clearSelection()
       console.log("paper & scissors = Player Two Wins!")
     } else {
       console.log("I don't know!")
@@ -64,42 +94,118 @@ function p2EnterName() {
   );
 }
 
-database.ref("players/one").on("value", function(snapshot) {
-  p1EnterName()
+function clearSelection() {
+  database.ref("players/one").update({
+    selection: "null"
+  });
+  database.ref("players/two").update({
+    selection: "null"
+  });
+}
+
+function tie() {
+  ties++;
+  database.ref("players/one").update({
+    tie: ties
+  });
+  database.ref("players/two").update({
+    tie: ties
+  });
+}
+
+function p1Win() {
+  p1Wins++;
+  p2Losses++;
+  database.ref("players/one").update({
+    win: p1Wins
+  })
+  database.ref("players/two").update({
+    loss: p2Losses
+  })
+}
+
+function p2Win() {
+  p2Wins++;
+  p1Losses++;
+  database.ref("players/one").update({
+    loss: p1Losses
+  })
+  database.ref("players/two").update({
+    win: p2Wins
+  })
+}
+
+database.ref("players/one").once("value", function(snapshot) {
   if (snapshot.exists()) {
-    if (snapshot.val().name !== "null") {
-      $("#player-one-name").html(snapshot.val().name);
-      $(document).on("click", "#p1-option", function(event) {
-        var selection = $(this);
-        $(this).css("background-color", "green");
-        selection = selection[0].children[0].alt;
-        console.log("selection", selection);
-        database.ref("players/one").set({
-          name: snapshot.val().name,
-          selection: selection
-        })
+    $("#player-one-name").html(snapshot.val().name);
+    $(document).on("click", "#p1-option", function(event) {
+      var selection = $(this);
+      $(this).css("background-color", "green");
+      selection = selection[0].children[0].alt;
+      console.log("selection", selection);
+      database.ref("players/one").update({
+        name: snapshot.val().name,
+        selection: selection
       })
-    } 
+      database.ref("players/two/selection").once("value", function(snapshot) {
+        if (snapshot.val() !== "null") {
+          determineWinner()
+        }
+      })
+    })
+  }
+})
+
+database.ref("players/two").once("value", function(snapshot) {
+  if (snapshot.exists()) {
+    $("#player-two-name").html(snapshot.val().name);
+    $(document).on("click", "#p2-option", function(event) {
+      var selection = $(this);
+      $(this).css("background-color", "green");
+      selection = selection[0].children[0].alt;
+      console.log("selection", selection);
+      database.ref("players/two").update({
+        name: snapshot.val().name,
+        selection: selection
+      })
+      database.ref("players/one/selection").once("value", function(snapshot) {
+        if (snapshot.val() !== "null") {
+          determineWinner()
+        }
+      })
+    })
+  }
+})
+
+database.ref("players/one").on("value", function(snapshot) {
+  if (!snapshot.exists()) {
+    console.log("No Player One!");
+    p1EnterName()
   }
 })
 
 database.ref("players/two").on("value", function(snapshot) {
-  p2EnterName()
+  if (!snapshot.exists()) {
+    console.log("No Player Two!");
+    p2EnterName()
+  }
+})
+
+database.ref("players/one").on("value", function(snapshot) {
   if (snapshot.exists()) {
-    if (snapshot.val().name !== "null") {
-      $("#player-two-name").html(snapshot.val().name);
-      $(document).on("click", "#p2-option", function(event) {
-        var selection = $(this);
-        $(this).css("background-color", "green");
-        selection = selection[0].children[0].alt;
-        console.log("selection", selection);
-        database.ref("players/two").set({
-          name: snapshot.val().name,
-          selection: selection
-        })
-        determineWinner()
-      })
-    } 
+    $("#player-one-name").html(snapshot.val().name);
+    $("#p1-win").html(snapshot.val().win);
+    $("#p1-tie").html(snapshot.val().tie);
+    $("#p1-loss").html(snapshot.val().loss);
+  }
+})
+
+database.ref("players/two").on("value", function(snapshot) {
+  if (snapshot.exists()) {
+    $("#player-two-name").html(snapshot.val().name);
+    $("#p2-win").html(snapshot.val().win);
+    $("#p2-tie").html(snapshot.val().tie);
+    $("#p2-loss").html(snapshot.val().loss);
   }
 })
 
@@ -109,15 +215,19 @@ $(document).on("click", "#p1-name-submit", function(event) {
   var playerOneName = $("[name*='p1-name']").val().trim(); 
   console.log("playerOneName", playerOneName);
   database.ref("players/one").set({
-    name: playerOneName
+    name: playerOneName,
+    selection: "null",
+    win: 0,
+    tie: 0,
+    loss: 0
   })
-  database.ref("players/two").once("value", function(snapshot) {
-    if (snapshot.exists()) {
-      $("#p2-waiting").html("");
-    } else {
-      $("#p1-waiting").html("Waiting for 'Player 2' to Join");
-    }
-  })
+  // database.ref("players/two").once("value", function(snapshot) {
+  //   if (snapshot.exists()) {
+  //     $("#p2-waiting").html("");
+  //   } else {
+  //     $("#p1-waiting").html("Waiting for 'Player 2' to Join");
+  //   }
+  // })
 });
 
 $(document).on("click", "#p2-name-submit", function(event) {
@@ -126,15 +236,19 @@ $(document).on("click", "#p2-name-submit", function(event) {
   var playerTwoName = $("[name*='p2-name']").val().trim(); 
   console.log("playerTwoName", playerTwoName);
   database.ref("players/two").set({
-    name: playerTwoName
+    name: playerTwoName,
+    selection: "null",
+    win: 0,
+    tie: 0,
+    loss: 0
   })
-  database.ref("players/one").once("value", function(snapshot) {
-    if (snapshot.exists()) {
-      $("#p1-waiting").html("");
-    } else {
-      $("#p2-waiting").html("Waiting for 'Player 1' to Join");
-    }
-  })
+  // database.ref("players/one").once("value", function(snapshot) {
+  //   if (snapshot.exists()) {
+  //     $("#p1-waiting").html("");
+  //   } else {
+  //     $("#p2-waiting").html("Waiting for 'Player 1' to Join");
+  //   }
+  // })
 });
 
 
